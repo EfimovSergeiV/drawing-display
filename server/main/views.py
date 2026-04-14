892303d4777ea14c7b438fb289ab46e1803cd397
EfimovSergeiV <efimovsergeiv@gmail.com>
2026-04-14 15:01:46 +0300

    add:

--- server/main/views.py
+++ server/main/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from django.core.files.uploadedfile import InMemoryUploadedFile
-
+from time import sleep
 
 
 class StatusView(APIView):
@@ -22,5 +22,6 @@ class ControlView(APIView):
         password = "123456"
         command = f"echo {password} | sudo -S /usr/sbin/shutdown now"
         os.system(f'echo "{password}" | sudo -S systemctl stop runner')
+        sleep(1)
         os.system(command)
         return Response(status=status.HTTP_200_OK)
